f4d0c170b17679679f2f67208748a61ba5335966
Wu-NaYa <150319210+Wu-NaYa@users.noreply.github.com>
2026-03-27 16:36:51 +0800

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -24,9 +24,7 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9229")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
